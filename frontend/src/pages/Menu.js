ff504da3a326342e214f88376737066dee7ca07e
Anna Tran <annatran@annas-air.mynet>
2020-10-15 11:58:31 +0100

    import menu card component

---
+++ frontend/src/pages/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MenuCard } from '../components'
+
+const Menu = () => {
+    return(
+        <>
+        <MenuCard/>
+        </>
+    )
+}
+
+export default Menu;
