58c6f2eb8b4ef2fc7f0a50425ac21e2eb074eba8
Anna Tran <annatran@annas-air.mynet>
2020-10-15 11:56:14 +0100

    first commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Baguette</h1>
+            <img src="https://images.unsplash.com/photo-1550507992-eb63ffee0847?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" className="banner" alt="Banner"/>
+            {/* icons next to images */}
+            {/* reviews */}
+        </div>
+    )
+}
+
+export default Home;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Account } from './Account'
+export { default as Menu } from './Menu'
+export { default as Basket } from './Basket'
+export { default as About } from './About'
+export { default as Home } from './Home'
