41ef9c5aa19caef21b69efa42e7ddf83379a060b
Anna Tran <annatran@annas-air.mynet>
2020-10-16 22:21:08 +0100

    add paths for current user and all users

--- backend/food/urls.py
+++ backend/food/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import current_user, UserList
 
 urlpatterns = [
-    path('', views.index, name="food-index")
+    path('menu/', views.index, name="food-index"),
+    path('current_user/', current_user),
+    path('users/', UserList.as_view())
 ]
