532479b675152a457bbddd8f0f95c8ebca1bff4f
Anna Tran <annatran@annas-air.mynet>
2020-10-18 15:20:52 +0100

    create Pay Now button

--- frontend/src/pages/Basket.js
+++ frontend/src/pages/Basket.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 
-const Basket = () => {
+const Basket = (props) => {
+
+    const handleClick = e => {
+        e.preventDefault()
+        props.history.push('/account')
+    }
+
     return (
         <div className="basket-container">
-
+            <button onClick={handleClick}>Pay Now</button>
         </div>
     )
 }
 
-export default Basket;
+export default withRouter(Basket);
