acd01c6e1bff26e990b88e73f36f4e6fe618b45f
Anna Tran <annatran@annas-air.mynet>
2020-10-18 15:21:13 +0100

    change photo, add text and social media icons

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import './styles/HomeStyle.css'
 
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>Baguette</h1>
-            <img src="https://images.unsplash.com/photo-1550507992-eb63ffee0847?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" className="banner" alt="Banner"/>
-            {/* icons next to images */}
-            {/* reviews */}
+            <div className="home-headings">
+                <h1 className="home-heading">Vieguette's</h1>
+                <h4 className="home-sub">London</h4>
+            </div>
+            <img src="https://images.unsplash.com/photo-1509722747041-616f39b57569?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" className="banner" alt="Banner"/>
+            <div className="icons">
+            <i className="fa fa-facebook-square" aria-hidden="true"></i><br/>
+            <i className="fa fa-instagram" aria-hidden="true"></i>
+            </div>
+            <div className="reviews">
+            <p className="review-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="review-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="review-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="review-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="review-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            </div>
         </div>
     )
 }
