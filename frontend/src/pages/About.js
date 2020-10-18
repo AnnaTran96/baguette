5e08e5d5d00ccc9a413538340986af80dcb44261
Anna Tran <annatran@annas-air.mynet>
2020-10-18 15:20:15 +0100

    add images and paragraphs

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import './styles/AboutStyle.css'
 
 const About = () => {
     return (
         <div className="about-container">
+            <div className="about-img-container">
+                <img src="https://images.unsplash.com/flagged/photo-1561668038-2742fcef75d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Baking" className="baking-img"></img>
+                <img src="https://images.unsplash.com/photo-1599819055803-717bba43890f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Baguette" className="baguette-img"></img>
+                <img src="https://images.unsplash.com/photo-1456765447332-d79762cdcbe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1488&q=80" alt="Baguette" className="baguette-img2"></img>
+            </div>
+            <h1 className="about-heading">About Us</h1>
+            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+            <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris rhoncus aenean vel elit scelerisque mauris. Congue mauris rhoncus aenean vel. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Luctus accumsan tortor posuere ac ut consequat semper. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Vestibulum lorem sed risus ultricies tristique. Nulla at volutpat diam ut venenatis tellus in metus vulputate. Nisi est sit amet facilisis magna. Laoreet sit amet cursus sit. Arcu dui vivamus arcu felis bibendum ut tristique. Diam sollicitudin tempor id eu.
+            </p>
+
 
         </div>
     )
