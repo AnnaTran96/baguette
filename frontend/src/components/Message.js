42bb0996bc1bb15d92de3d2c78f857aff13cb989
Anna Tran <annatran@annas-air.mynet>
2020-10-19 20:53:26 +0100

    processing order message

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Message = () => {
+
+    return (
+       <>
+       <h3>Thank you for placing an order with us.</h3>
+       <p>It will be with you very shortly!</p>
+       </>
+    )
+}
+
+export default Message;
