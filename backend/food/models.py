3e79036108e875d828ed1ec893bacee1af219a56
Anna Tran <annatran@annas-air.mynet>
2020-10-19 11:30:39 +0100

    alter data type

--- backend/food/models.py
+++ backend/food/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Baguette(models.Model):
     name = models.CharField(max_length=100)
     ingredients = models.CharField(max_length=300)
-    price = models.PositiveIntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=0)
     img_src = models.CharField(max_length=400, default='')
 
     def __str__(self):
