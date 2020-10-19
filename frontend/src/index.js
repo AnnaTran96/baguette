b5ba7425622cbe70b8ebfae051cecd28c609d702
Anna Tran <annatran@annas-air.mynet>
2020-10-19 20:50:01 +0100

    update reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
-// import reducer from './reducer/reducer'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import basketReducer from './reducer/basketReducer'
 
-// const store = createStore(reducer)
+const store = createStore(basketReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
