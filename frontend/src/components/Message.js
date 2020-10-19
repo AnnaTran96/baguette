33cd4c2c3b7e2c7f2f2671832b9dd2a555e02b09
Anna Tran <annatran@annas-air.mynet>
2020-10-19 20:55:09 +0100

    update content

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -5,7 +5,7 @@ const Message = () => {
     return (
        <>
        <h3>Thank you for placing an order with us.</h3>
-       <p>It will be with you very shortly!</p>
+       <p>We are currently processing your order. It will be with you very shortly!</p>
        </>
     )
 }
