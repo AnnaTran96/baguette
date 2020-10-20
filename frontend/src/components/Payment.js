dbca1dc5114990fac89e7acd36d8775c13dbd122
Anna Tran <annatran@annas-air.mynet>
2020-10-20 20:39:07 +0100

    add classnames to input

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import './styles/PaymentStyle.css'
 
 const Payment = (props) => {
 
@@ -9,22 +10,24 @@ const Payment = (props) => {
     }
 
     return (
-       <div className="payment-container">
-           <h2>Payment</h2>
-           <p>Please enter your card details below</p>
-           <form>
-               <label for="card-name">Card Name
-                   <input type="text" for="card-name" placeholder="Card Name"/>
-               </label>
-               <label for="card-number">Card Number
-                   <input type="text" for="card-number" placeholder="0000 0000 0000 0000" maxLength="19"/>
-               </label>
-               <label for="card-expiry">Expiry Date
-                   <input type="text" for="card-expiry" placeholder="00/00" maxLength="5"/>
-               </label>
-           </form>
+        <>
+        <div className="payment-container">
+            <h2>Payment</h2>
+            <p>Please enter your card details below</p>
+            <form>
+                <label for="card-name">Card Name
+                    <input type="text" for="card-name" placeholder="Card Name" className="payment-input"/>
+                </label>
+                <label for="card-number">Card Number
+                    <input type="text" for="card-number" placeholder="0000 0000 0000 0000" maxLength="19" className="payment-input"/>
+                </label>
+                <label for="card-expiry">Expiry Date
+                    <input type="text" for="card-expiry" placeholder="00/00" maxLength="5" className="payment-input"/>
+                </label>
+            </form>
+        </div>
         <button onClick={handleClick} className="payBtn">Pay Now</button>
-       </div>
+       </>
     )
 }
 
