98f7a95cb43992fb7da609bb34628e30e744323e
Anna Tran <annatran@annas-air.mynet>
2020-10-22 15:55:39 +0100

    test it has a state

--- frontend/src/__test__/components/Signup.test.js
+++ frontend/src/__test__/components/Signup.test.js
@@ -1,9 +1,12 @@
 import Signup from '../../components/Signup';
 import React from 'react'
 import { shallow } from 'enzyme';
+// import fetchMock from 'fetch-mock'
 
 describe('Signup', () => {
     let wrapper;
+    let fakeEvent = { preventDefault: () => 'fake'}
+    const fakeResponse = {"token": ""}
     const match={params: {username: 'anna'}, isExact: true, path: "", url: ""}
     const fetchUserFn = jest.fn(match);
     
@@ -11,6 +14,13 @@ describe('Signup', () => {
         wrapper = shallow(<Signup match={match} fetchUserFn={fetchUserFn} />);
     })
 
+    // afterEach(() => {
+    //     fetchMock.restore()
+    // })
+
+    test('it renders', () => {
+        expect(wrapper.find('.signup-container')).toHaveLength(1);
+    });
     test('it should call', () => {
         expect(wrapper.containsMatchingElement(<h3>New User? Register Below</h3>)).toBeTruthy()
     })
@@ -19,4 +29,18 @@ describe('Signup', () => {
         const instance = wrapper.instance()
         expect(instance['state']).toEqual({"email": "", "password": "", "passwordTwo": "", "username": ""})
     })
+
+    // test('it handles input', () => {
+    //     let input = wrapper.find('input').at(0)
+    //     let instance = wrapper.instance()
+    //     input.simulate('change', fakeEvent)
+    //     expect(instance['state']).toEqual({"username": "", "password": ""})
+    // })
+    
+    // test('it fetches data from the server', () => {
+    //     fetchMock.getOnce('/token-auth', {
+    //         body: {username: "", password: ''},
+    //         headers: {"Content-type": "application/json", 'Authorization': "Token" }
+    //     })
+    // })
 });
